089d0c08c83886ef7460ae6b480b54313c3d9db5
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-11 18:11:33 +0100

    added links in home page

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: String,
-  email: {type: String, unique: true},
+  email: { type: String, unique: true },
+  role: { type: String, enum: ["ADMIN", "STUDENT"] },
   password: String,
   passwordReset: String,
   _course: Schema.Types.ObjectId
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
