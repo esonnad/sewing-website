38e016a4229175f7c67f464a1208b7b5bf535496
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-11 17:33:32 +0100

    Added post model

--- models/Course.js
+++ models/Course.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
   name: String,
-  _students: [Schema.Types.ObjectId]
+  _students: [Schema.Types.ObjectId],
+  dates: [Object]
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
 
 const Course = mongoose.model('Course', courseSchema);
 module.exports = Course;
---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  header: String,
+  content: String,
+  imgPath: String,
+  imgName: String,
+  _creator: Schema.Types.ObjectId,
+}, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
