304c51fee11b6bc2b60edde50a3227191ef51ee6
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-11 17:37:18 +0100

    Merged2

--- models/Course.js
+++ models/Course.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
-<<<<<<< HEAD
-  courseName: String,
-  _students: [Schema.Types.ObjectId]
-=======
   name: String,
   _students: [Schema.Types.ObjectId],
   dates: [Object]
->>>>>>> 38e016a4229175f7c67f464a1208b7b5bf535496
 }, {
     timestamps: {
       createdAt: 'created_at',
